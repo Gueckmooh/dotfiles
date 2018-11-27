af91019fe128efe03d5543aef79e9d5fb6e758c7
gueckmooh <gueckmooh@pm.me>
2018-11-28 00:09:29 +0100

    modifs encore

--- conky/.config/conky/conky.sh
+++ conky/.config/conky/conky.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cava -p "$HOME/.config/cava/raw" &
-conky -c "$HOME/.config/conky/cava" -d
-conky -c "$HOME/.config/conky/notes" -d
-conky -d
+conky -c "$HOME/.config/conky/cava" -d > /dev/null
+conky -c "$HOME/.config/conky/notes" -d > /dev/null
+conky -d > /dev/null
