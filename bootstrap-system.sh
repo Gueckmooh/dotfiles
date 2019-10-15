c68e5072e2b5b3450c0d1335024b81ac7dfd923c
gueckmooh <gueckmooh@pm.me>
2019-10-15 20:59:20 +0200

    test 2

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo Hello
+info () {
+    printf "\r  [ \033[00;34minfo\033[0m ] $1\n"
+}
+
+user () {
+    printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+    echo ''
+    exit
+}
+
+info "Hello"
