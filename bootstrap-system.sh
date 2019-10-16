196a89d9bbe971c95a49a8aee0d31756c34116e0
gueckmooh <gueckmooh@pm.me>
2019-10-16 17:08:55 +0200

    Change bootstrap

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -21,6 +21,7 @@ fail () {
 info "First of all install some packages"
 sudo pacman -S --needed base-devel git man stow emacs xorg-server xorg-xinit mesa xterm xorg-twm awesome gdm xorg-server-xephyr opam python-pip ttf-dejavu ttf-droid ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-hack ttf-inconsolata ttf-liberation acpi luarocks mpd ncmpcpp zathura rofi redshift lolcat cowsay thefuck figlet firefox neofetch compton htop fortune-mod network-manager-applet scrot lxappearance offlineimap neomutt notmuch pass bash-completion
 
+
 info "Install pikaur"
 mkdir -p ~/git/
 git clone https://aur.archlinux.org/pikaur.git ~/git/pikaur
@@ -48,6 +49,39 @@ fi
 git clone --single-branch --branch=testing --recursive https://github.com/gueckmooh/yet-another-awesome-config ~/.config/awesome
 git clone https://github.com/gueckmooh/.emacs.d ~/.emacs.d
 
-# gpg --full-gen-key -> pass
+gpg --full-gen-key
+user ' - What is your email?'
+read -e email
+pass init "$email"
 
 sudo systemctl enable gdm
+OLDPATH="$PWD"
+
+cd ~/.dotfiles
+bash bootstrap.sh
+for file in ~/.bash*
+do
+    info "Savind $file -> $file.old"
+    mv "$file" "$file.old"
+done
+
+function __stow() {
+    info "Stowing $1"
+    stow "$1"
+}
+
+__stow bash
+__stow git
+__stow cows
+__stow compton
+__stow htop
+__stow mailcap
+__stow mpd
+__stow ncmpcpp
+__stow redshift
+__stow rofi
+__stow termite
+__stow zathura
+
+info "Bootstraping ended"
+neofetch
