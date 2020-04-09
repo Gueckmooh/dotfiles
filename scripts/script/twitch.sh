88c429b635be1dc459bc49ce6f87f52713f0c609
Gueckmooh <gueckmooh@pm.me>
2020-04-09 17:36:37 +0200

    Some fixes + improvements

--- scripts/script/twitch.sh
+++ scripts/script/twitch.sh
@@ -2,6 +2,26 @@
 
 # Il faut remplacer "$CLIENTID" par le votre (voir l'API twitch)
 
+# GETOPT
+usage()
+{
+    echo "Usage: $0 [-q query]" 2>&1
+    exit 1
+}
+
+while getopts ":q" opt
+do
+    case "$opt" in
+        q)
+            query=1
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift "$((OPTIND - 1))"
+
 get_user_info()
 {
     name="$1"
@@ -31,7 +51,6 @@ get_logo()
     done
     shift "$((OPTIND - 1))"
     name="$1"
-    echo "output = $output"
     logo_url=$(get_user_info "$name" | jq -r '.logo')
 
     if [[ -z "$output" ]]
@@ -39,9 +58,33 @@ get_logo()
         echo "$logo_url"
     else
         output="$output.${logo_url##*.}"
-        wget -O "$output" "$logo_url"
+        wget -q -O "$output" "$logo_url"
         echo "$output"
     fi
 }
+
+get_stream_info()
+{
+    name="$1"
+    id=$(get_id "$name")
+    curl -s -H 'Accept: application/vnd.twitchtv.v5+json' -H "Client-ID: $CLIENTID" -X GET "https://api.twitch.tv/kraken/streams/$id" | jq ".stream"
+}
+
+is_live()
+{
+    name="$1"
+    if [[ $(get_stream_info "$name") == "null" ]]
+    then
+        echo "no"
+    else
+        echo "yes"
+    fi
+}
+
+if [[ ! -z "$query" ]]
+then
+    eval "$*"
+    exit 0
+fi
 
 get_logo -a gexdragon
